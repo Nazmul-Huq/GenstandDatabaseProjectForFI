da2c831aafa9cbee52b41a200a3189a60bc6a2e3
Nazmul-Huq <hmnazmulhuq@gmail.com>
2023-12-04 16:25:14 +0100

    Database created successfully

--- GenstandDatabaseProjectForFI/GenstandDatabaseProjectForFI/Data/ApplicationDbContext.cs
+++ GenstandDatabaseProjectForFI/GenstandDatabaseProjectForFI/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SharedLibrary.Models;
 
 namespace GenstandDatabaseProjectForFI.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Genstand> Genstands { get; set; }
     }
 }
---
+++ GenstandDatabaseProjectForFI/GenstandDatabaseProjectForFI/Data/Migrations/20231204152406_FirstDatabaseCreation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GenstandDatabaseProjectForFI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstDatabaseCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genstands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genstands", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Genstands");
+        }
+    }
+}
