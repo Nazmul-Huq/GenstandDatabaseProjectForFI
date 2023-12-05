fab00be8851e50c0fd83946727477f83dd6262d7
Nazmul-Huq <hmnazmulhuq@gmail.com>
2023-12-05 19:52:37 +0100

    fix the repository and form

--- GenstandDatabaseProjectForFI/GenstandDatabaseProjectForFI/Repositories/GenstandRepository.cs
+++ GenstandDatabaseProjectForFI/GenstandDatabaseProjectForFI/Repositories/GenstandRepository.cs
@@ -16,8 +16,8 @@ namespace GenstandDatabaseProjectForFI.Repositories
         public async Task<Genstand> AddProductAsync(Genstand model)
         {
             if (model is null) return null!;
-            var chk = await applicationDbContext.Genstands.Where(g => g.Name.ToLower().Equals(model.Name.ToLower())).FirstOrDefaultAsync();
-            if (chk is not null) return null!;
+            //var chk = await applicationDbContext.Genstands.Where(g => g.Name.ToLower().Equals(model.Name.ToLower())).FirstOrDefaultAsync();
+            //if (chk is not null) return null!;
 
             var newDataAdded = applicationDbContext.Genstands.Add(model).Entity;
             await applicationDbContext.SaveChangesAsync();
