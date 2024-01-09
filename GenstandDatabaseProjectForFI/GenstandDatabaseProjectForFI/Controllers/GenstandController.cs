4a369645bcabf98c7afe3246d7751fba767aa117
Nazmul-Huq <hmnazmulhuq@gmail.com>
2024-01-09 17:19:14 +0100

    image upload ok

--- GenstandDatabaseProjectForFI/GenstandDatabaseProjectForFI/Controllers/GenstandController.cs
+++ GenstandDatabaseProjectForFI/GenstandDatabaseProjectForFI/Controllers/GenstandController.cs
@@ -1,9 +1,12 @@
 ﻿using GenstandDatabaseProjectForFI.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using SharedLibrary.Dtos;
 using SharedLibrary.Interfaces;
 using SharedLibrary.Models;
+using System.Collections.Generic;
+using System.Net;
 
 namespace GenstandDatabaseProjectForFI.Controllers
 {
@@ -13,10 +16,12 @@ namespace GenstandDatabaseProjectForFI.Controllers
     {
         private readonly IGenstandOperations genstandOperations;
         private readonly IFilmOperations filmOperations;
-        public GenstandController(IGenstandOperations genstandOperations, IFilmOperations filmOperations)
+        private readonly IHostEnvironment env;
+        public GenstandController(IGenstandOperations genstandOperations, IFilmOperations filmOperations, IHostEnvironment env)
         {
             this.genstandOperations = genstandOperations;
             this.filmOperations = filmOperations;
+            this.env = env;
         }
 
         [HttpPost("Add-Genstand")]
@@ -24,7 +29,8 @@ namespace GenstandDatabaseProjectForFI.Controllers
         {
             if (model == null) return BadRequest(ModelState);
             Film film = await filmOperations.GetFilmByIdAsync(model.FilmId);
-            Genstand genstandToSave = new Genstand { 
+            Genstand genstandToSave = new Genstand
+            {
                 Name = model.Name,
                 Description = model.Description,
                 DateOfGenstand = model.DateOfGenstand,
@@ -85,5 +91,64 @@ namespace GenstandDatabaseProjectForFI.Controllers
             return Ok(genstands);
         }
 
+        [HttpPost("Add-Image")]
+        public async Task<ActionResult<IList<ImageUploadResult>>> PostFile([FromForm] IEnumerable<IFormFile> files)
+        {
+            var uploadResult = new ImageUploadResult();
+            IList<ImageUploadResult> uploadResults = new List<ImageUploadResult>();
+
+
+
+
+            foreach (var file in files)
+            {
+                var untrustedFileName = file.FileName;
+                //uploadResult.FileName = untrustedFileName;
+                var trustedFileNameForDisplay = WebUtility.HtmlEncode(untrustedFileName);
+
+
+                if (file.Length == 0)
+                {
+                    uploadResults.Add(new ImageUploadResult { success = false });
+                }
+                else
+                {
+                    try
+                    {
+                        var directoryPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
+                        var fullPath = Path.Combine(directoryPath, trustedFileNameForDisplay);
+
+                        // Maek sure the directory is ther bycreating it if it's not exist
+                        Directory.CreateDirectory(directoryPath);
+
+                        var trustedFileNameForFileStorage = Path.GetRandomFileName();
+                        var path = Path.Combine(env.ContentRootPath,
+                            env.EnvironmentName, "unsafe_uploads",
+                            trustedFileNameForFileStorage);
+
+                        //await using FileStream fs = new(path, FileMode.Create);
+                        await using FileStream fs = new(fullPath, FileMode.Create, FileAccess.Write);
+                        await file.CopyToAsync(fs);
+
+                        //logger.LogInformation("{FileName} saved at {Path}", trustedFileNameForDisplay, path);
+                        //uploadResult.Uploaded = true;
+                        // uploadResult.StoredFileName = trustedFileNameForFileStorage;
+                    }
+                    catch (IOException ex)
+                    {
+                        //trustedFileNameForDisplay, ex.Message);
+                        //uploadResult.ErrorCode = 3;
+                    }
+
+                }
+
+                //if (files.Length == 0) return BadRequest(new ImageUploadResult { success = false });
+
+                //return Ok(new ImageUploadResult { success = true });
+                
+
+            }
+            return new CreatedResult();
+        }
     }
 }
